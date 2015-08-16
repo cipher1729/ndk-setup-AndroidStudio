533c8535d9f1d680454cc134f90de14c1133fdca
cipher1729 <cipher1729@gmail.com>
2015-08-16 01:29:49 -0700

    Using Android.mk and empty Application.mk

---
+++ ndktest/src/main/obj/local/armeabi/objs/ndktest/main.o.d
@@ -0,0 +1,17 @@
+C:/Users/cipher1729/testNDK/ndktest/src/main/obj/local/armeabi/objs/ndktest/main.o: \
+ C:/Users/cipher1729/testNDK/ndktest/src/main/jni/main.c \
+ C:/Users/cipher1729/testNDK/ndktest/src/main/jni/main.h \
+ C:/Users/cipher1729/Desktop/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/jni.h \
+ C:/Users/cipher1729/Desktop/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ C:/Users/cipher1729/Desktop/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/Users/cipher1729/Desktop/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h
+
+C:/Users/cipher1729/testNDK/ndktest/src/main/jni/main.h:
+
+C:/Users/cipher1729/Desktop/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/jni.h:
+
+C:/Users/cipher1729/Desktop/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+C:/Users/cipher1729/Desktop/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/Users/cipher1729/Desktop/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h:
